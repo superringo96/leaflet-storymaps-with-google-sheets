3d19483afc4ad16a1b480b9e7899985414124d14
superringo96 <62756681+superringo96@users.noreply.github.com>
2020-03-27 16:58:56 +0200

    Change of map

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1W4VY-F48HXfHB7Y5B46oXPRylrRdDCc3m6blWrH8nKs/edit#gid=0';
